d29ba701de674d6814af039d3c74169a4566821a
snutt10 <snutt@student.neumont.edu>
2026-02-22 23:34:39 -0700

    configuring prometheus

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const client = require('prom-client');
 const swaggerUi = require('swagger-ui-express');
 const {setupSwagger} = require('./config/swagger');
 const {swaggerOptions} = require('./config/swagger');
@@ -6,8 +7,18 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const { connectDB } = require('./config/db');
 const { connectKafka } = require('./config/producer');
 const app = express();
+const register = new client.Registry();
 const PORT = process.env.PORT || 3000;
 
+client.collectDefaultMetrics({ register });
+
+const httpDuration = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.1, 0.5, 1, 2, 5],
+    registers: [register],
+});
 // Middleware
 app.use(express.json());
 
@@ -31,6 +42,8 @@ const startServer = async () => {
 
 setupSwagger(app);
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // ============================================
@@ -46,10 +59,23 @@ app.use('/exchanges', require('./routes/exchange'));
 // ============================================
 
 app.use((err, req, res, next) => {
+    const end = httpDuration.startTimer();
+    res.on('finish', () => {
+        end({ method: req.method, route: req.route ? req.route.path : req.path, status_code: res.statusCode });
+    });
+    next();
     console.error(err.stack);
     res.status(500).json({ error: 'Internal server error' });
 });
 
+app.get('/metrics', async (req, res) => {
+    try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+    } catch (error) {
+        res.status(500).end(error);
+    }
+});
 // ============================================
 // START SERVER
 // ============================================
